cddd03a1b2d81b840f80a756ed69466e43ad5066
munir sebikaaka <munirsebikaaka@gmail.com>
2025-03-26 23:29:13 +0300

    feat: added the global styles component to the application that gona save all the mani styles to the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: bold;
+  color: blue;
+  text-transform: capitalize;
+  background-color: #f0f0f0;
+`;
+const Button = styled.button`
+  display: inline-block;
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  cursor: pointer;
+  border-radius: 0.4rem;
+  background-color: #f0f0f0;
+  color: #333;
+  box-shadow: 0 0 10px black;
+`;
+const Input = styled.input`
+  padding: 1.2rem 1.6rem;
+  border: 1px solid #ccc;
+  background-color: #f0f0f0;
+  color: #333;
+  box-shadow: 0 0 10px black;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <GlobalStyles />
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <H1>the wild oasis</H1>
+        <Button>click me</Button>
+        <Input type="text" placeholder="type here" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
