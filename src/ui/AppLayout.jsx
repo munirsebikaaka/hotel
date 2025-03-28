cc49b1b9b9bd3f4e2b8ac6c8364f956b1987db9a
munir sebikaaka <munirsebikaaka@gmail.com>
2025-03-28 20:05:52 +0300

    feat:added the supabase ribary to the application

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,16 +1,28 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Sidebar from "./SideBar";
+import styled from "styled-components";
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  background-color: #f9fafb;
+  padding: 4rem 4.8rem 6.4rem;
+`;
 
 function AppLayout() {
   return (
-    <div>
+    <StyledAppLayout>
       <Header />
       <Sidebar />
-      <main>
+      <Main>
         <Outlet />
-      </main>
-    </div>
+      </Main>
+    </StyledAppLayout>
   );
 }
 export default AppLayout;
